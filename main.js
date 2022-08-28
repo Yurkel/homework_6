0e5ac955d6d3b61b2ef9c6057a11f771b5919ac4
Yuriy <yuriy-k@ukr.net>
2022-08-28 17:08:46 +0300

    all done

---
+++ main.js
@@ -0,0 +1,30 @@
+const store = {
+  storeName: "Курочка Ряба",
+  stoteAdress: {
+    storeCountry: "Ukraine",
+    storeCity: "Odessa",
+    storeStreat: "Deribasovskaya",
+    storeHouse: 3,
+  },
+};
+const clients = {};
+clients["Bohdan Onatskyi"] = {
+  quantity: 5,
+  bonuses: 50,
+};
+clients["Александр Куликов"] = {
+  quantity: 12,
+  bonuses: 120,
+};
+clients["Yuriy Kruchinin"] = {
+  quantity: 3,
+  bonuses: 30,
+};
+clients["Олена Кузьмичова"] = {
+  quantity: 1,
+  bonuses: 10,
+};
+clients["Тетяна Затолока"] = {
+  quantity: 10,
+  bonuses: 100,
+};
